fad1c9f59030215963b3505ef98067a3577a4e49
Dan Zera <dczera@gmail.com>
2020-05-03 23:23:43 -0700

    initialize state of SearchBar component, handle changes to input via callback function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,15 +3,27 @@ import React from 'react';
 // SearchBar needs to be a class based component
 // so we can utilize state for handling user input
 class SearchBar extends React.Component {
+	// state initilization
+	state = {
+		searchTerm: ''
+	};
 
+	// this function gets passed by reference below as a callback function
+	// this is one general naming convention for event handlers
+	onInputChange = (e) => {
+		this.setState({ searchTerm: e.target.value });
+	}
 
+	// onChange callback function automatically invoked any time the input is updated
 	render() {
 		return (
 			<div className="search-bar ui segment">
 				<form className="ui form">
 					<div className="field">
 						<label>Image Search</label>
-						<input type="text" />
+						<input type="text"
+										value={this.state.searchTerm}
+										onChange={this.onInputChange} />
 					</div>
 				</form>
 			</div>
