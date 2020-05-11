e04c26ee6d09a5bf67dfd5c25bb29e222c4f58a2
Dan Zera <dczera@gmail.com>
2020-05-11 16:04:44 -0700

    update ImageList component to render the list of image returned from Unsplash API search results

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 
 const ImageList = props => {
-	console.log(props.images)
+	// use destructuring to pull properties off of the image object
+	const images = props.images.map(({description, id, urls}) => {
+		return <img alt={description} key={id} src={urls.regular} />
+	});
 
 	return (
 		<div className="image-list">
-			ImageList
+			{images}
 		</div>
-	);
-
+	);	
 }
 
 export default ImageList;
