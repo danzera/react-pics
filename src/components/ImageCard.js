56d339475b5e18f8fd8af017019263cd922c1a1c
Dan Zera <dczera@gmail.com>
2020-05-11 17:31:01 -0700

    add helper method to determine height of each image and set the state/grid height accordingly

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,40 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			spans: 0
+		}
+
+		// React Refs give access to a single DOM element
+		// refs are created in the constructor and assigned to instance variables
+		// they are then passed to a particular JSX element as props
+		this.imageRef = React.createRef();
+	}
+
+	setSpans = e => {
+		const SPAN_HEIGHT = 10; // defined in ImageCard.css
+		const imageHeight = this.imageRef.current.clientHeight;
+		const spans = Math.ceil(imageHeight/SPAN_HEIGHT);
+
+		this.setState({ spans });
+	}
+
+	componentDidMount() {
+		// add event listener to the image
+		this.imageRef.current.addEventListener('load', this.setSpans);
+	}
+
 	render() {
 		const { description, urls } = this.props.image;
 
 		return (
-			<div className="image-card">
-				<img alt={description}
+			<div className="image-card" style={{gridRowEnd: `span ${this.state.spans}`}}>
+				<img 
+					alt={description}
+					ref={this.imageRef}
 					src={urls.regular} 
 				/>
 			</div>
